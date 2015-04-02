df6925d967ada300777908ede67130c496c536af
vishal <vishal41190@gmail.com>
2015-04-01 18:41:11 -0700

    Url genereation works.

--- app.js
+++ app.js
@@ -15,26 +15,66 @@ client.on("error",function(err){
     console.log("Error" + err);
 });
 
+var longurl;
+
+var islongUrlExist= function(longurl,callback){
+     client.hget("sort1","longurl",function(err,response){
+            callback(err,response);
+        }); 
+};
+
 app.get('/getUrl',function(req,res){
     var url = req.query.url;
     var index = url.indexOf("localhost:3000");
-    if(index<8)
+    if(index>-1 && index<8)
     {
-        //shorturl
+        //sorturl
+        client.hget(url,"longurl",function(err,response){
+            res.json({"url":response});
+            
+        });
     }
     else{
         //longurl
-        var dataset = {
-        "shortUrl":
-        "longUrl":
-        "visit":
-        }
+        client.hget(url,"sorturl",function(err,response){
+            if(response===null)
+            {
+                //LongUrl not yet exist
+                //create new url 
+                var sorturl ="localhost:3000/sorturl3";
+                client.hset(url,"sorturl",sorturl,redis.print);
+                client.hset(sorturl,"longurl",url,redis.print);
+                client.zadd("views",1,sorturl,redis.print);
+                res.json({"url":sorturl});
+            }
+            else{
+                //LongUrl already exit
+                res.json({"url":response});
+            }
+        });
         
-        client.set("test","test",redis.print);
+             
+        //client.set("test","test",redis.print);
+
     }
-    
-//client.set("test","test",redis.print);
-res.end("get url called");
+   
+
 });
+
+app.get('/:url',function(req,res){
+    var url = req.params.url;
+    url="localhost:3000/"+url;
+    client.hget(url,"longurl",function(err,response){
+        if(response===null){
+            res.status(404).send("Url not exist");
+        }
+        else{
+        
+            res.redirect(response);
+        }
+    });
+    
+});
+
 
 
