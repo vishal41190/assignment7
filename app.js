8d046a48d8aa568b4e96563a00dc13f96cd93ca1
vishal <vishal41190@gmail.com>
2015-04-06 13:54:43 -0700

    Assignment done. Added Top 10 list

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var http= require('http');
+var http = require('http');
 var path = require('path');
-var redis= require('redis');
+var redis = require('redis');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -9,79 +9,84 @@ client = redis.createClient();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 http.createServer(app).listen(3000);
 
-client.on("error",function(err){
+client.on("error", function(err) {
     console.log("Error" + err);
 });
 
 var longurl;
 
-var islongUrlExist= function(longurl,callback){
-     client.hget("sort1","longurl",function(err,response){
-            callback(err,response);
-        }); 
+var islongUrlExist = function(longurl, callback) {
+    client.hget("sort1", "longurl", function(err, response) {
+        callback(err, response);
+    });
 };
 
-function generateSortUrl(){
+function generateSortUrl() {
     var temp = Date.now();
-    return(temp.toString(36));
-    
+    return (temp.toString(36));
+
 }
-app.post('/getUrl',function(req,res){
+app.post('/getUrl', function(req, res) {
     var url = req.body.url1;
     var index = url.indexOf("localhost:3000");
-    if(index>-1 && index<8)
-    {
+    if (index > -1 && index < 8) {
         //sorturl
-        client.hget(url,"longurl",function(err,response){
-            res.json({"url":response});
-            
+        client.hget(url, "longurl", function(err, response) {
+            res.json({
+                "url": response
+            });
+
         });
-    }
-    else{
+    } else {
         //longurl
-        client.hget(url,"sorturl",function(err,response){
-            if(response===null)
-            {
+        client.hget(url, "sorturl", function(err, response) {
+            if (response === null) {
                 //LongUrl not yet exist
                 //create new url 
                 var sorturl = generateSortUrl();
-                sorturl="localhost:3000/"+sorturl;
-                client.hset(url,"sorturl",sorturl,redis.print);
-                client.hset(sorturl,"longurl",url,redis.print);
-                client.zadd("views",1,sorturl,redis.print);
-                res.json({"url":sorturl});
-            }
-            else{
-                //LongUrl already exit
-                res.json({"url":response});
+                sorturl = "localhost:3000/" + sorturl;
+                client.hset(url, "sorturl", sorturl, redis.print);
+                client.hset(sorturl, "longurl", url, redis.print);
+                client.zincrby("views", 1, sorturl, redis.print);
+                res.json({
+                    "url": sorturl
+                });
+            } else {
+                //LongUrl already exital
+                res.json({
+                    "url": response
+                });
             }
         });
-        
-             
-        //client.set("test","test",redis.print);
 
     }
-   
+
 
 });
+app.get('/gettop', function(req, res) {
 
-app.get('/:url',function(req,res){
+    client.zrevrangebyscore("views", "+inf", 0, "limit", 0, 9, function(err, response) {
+        res.json({
+            "top": response
+        });
+    });
+
+});
+app.get('/:url', function(req, res) {
     var url = req.params.url;
-    url="localhost:3000/"+url;
-    client.hget(url,"longurl",function(err,response){
-        if(response===null){
+    url = "localhost:3000/" + url;
+    client.hget(url, "longurl", function(err, response) {
+        if (response === null) {
             res.status(404).send("Url not exist");
-        }
-        else{
-        
+        } else {
+            client.zincrby("views", 1, url, redis.print);
             res.redirect(response);
         }
     });
-    
+
 });
-
-
-
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,36 +1,61 @@
-$(document).ready(function(){
-    
-    $("#button").click(function(){
-    var url = $("#url").val();
-        if(url===undefined)
-        {
+$(document).ready(function() {
+
+    $("#button").click(function() {
+        var url = $("#url").val();
+        if (url === undefined) {
             alert("Please enter url in text box");
+        } else {
+            var UserUrl = JSON.stringify({
+                url1: url
+            });
+            $.ajax({
+
+                    type: "POST",
+                    url: "/getUrl",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    data: UserUrl
+                })
+                .done(function(data, status) {
+
+                    $('#result').html("");
+                    $('#result').append("<a href=" + data.url + ">" + data.url + "</a>");
+
+
+                })
+                .fail(function(data, status) {
+                    console.log("fail called");
+                    console.log(data);
+                    console.log(status);
+                });
+
         }
-        else{
-           var  UserUrl=JSON.stringify({url1:url});
-             $.ajax({
-            
-            type: "POST",
-            url: "/getUrl",
+
+    });
+
+    $.ajax({
+
+            type: "get",
+            url: "/gettop",
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            data: UserUrl
+
         })
-        .done(function (data,status){
-        
-            $('#result').html("");
-            $('#result').append("<a href="+data.url+">"+data.url+"</a>");
-                  
-               
+        .done(function(data, status) {
+            var i = 0;
+            console.log(data.top.length);
+            for (i = 0; i < data.top.length; i++) {
+                $(".popular").append("<a href=" + data.top[i] + ">" + data.top[i] + "</a><br>");
+            }
+
+
+
         })
-        .fail(function (data, status){
+        .fail(function(data, status) {
             console.log("fail called");
             console.log(data);
             console.log(status);
         });
-        
-        }
-    
-    });
-    
+
+
 });
